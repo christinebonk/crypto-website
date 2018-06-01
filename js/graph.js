8c7f174604f174b18a05eb5fb244262e6eb85717
Rye-Guy <ryanracioppo95@gmail.com>
2018-06-01 16:41:37 -0400

    articles & daily high added

--- js/graph.js
+++ js/graph.js
@@ -1,6 +1,4 @@
 
-
-
 function cryptoCompareData(symbol1, symbol2, exchange) {
 	var limit = "&limit=" + 30;
 	var queryURL = "https://min-api.cryptocompare.com/data/histoday?" + "fsym=" + symbol1 + "&tsym=" + symbol2 + limit + "&e=" + exchange + "&tryConversion=false"; 
@@ -10,14 +8,20 @@ function cryptoCompareData(symbol1, symbol2, exchange) {
 		url: queryURL,
 		method: "GET" 
 	}).then(function(response) {
+        dailyHigh = response.Data['30'].high;
+        $("#statHigh").text("$ " + dailyHigh + " CAD");
+        console.log(dailyHigh);
 		console.log(response.Data);
 		parsedData = parseDataCompare(response.Data);
-		console.log(parsedData);
-      //  calculateVolatility(parsedData);
-	}).then(function() {
+       // console.log(parsedData);
         drawChart(parsedData);
+        calculateVolatility(parsedData);
+	}).then(function() {
+
 	});
 };
+
+
 
 
 
@@ -35,7 +39,7 @@ function parseDataCompare(data) {
 
 
 function drawChart(data) {
-    var svgWidth = 600, svgHeight = 400;
+    var svgWidth = 700, svgHeight = 400;
     var margin = { top: 20, right: 20, bottom: 30, left: 50 };
     var width = svgWidth - margin.left - margin.right;
     var height = svgHeight - margin.top - margin.bottom;
@@ -87,4 +91,61 @@ function drawChart(data) {
 
 cryptoCompareData("BTC","CAD","QUADRIGACX");
 //var parsedData = parseData(a);
+
+
+function makeArticles(type) {
+    var queryURL = 'https://newsapi.org/v2/everything?' +
+    'q='+ type + '&' +
+    'from=2018-01-01&' +
+    'sortBy=popularity&' +
+    'apiKey=c91847ec964243038db42e65ab4c8169';
+      //var req = new Request(queryURL);
+   //var req = new Request(url);
+   var limit = 3;
+   $.ajax({
+    url: queryURL,
+    method: "GET"
+   }).then(function (response) {
+    var results = response.articles;
+   
+            for(var i = 0; i <limit; i++){
+      var res=results[i];
+      console.log(res);
+      var cryptoDiv = $('<div class="article__item">');
+      var cryptImage = $('<img class="article__image">');
+      cryptImage.attr("src", res.urlToImage);
+      var crypto1 = res.title;
+      var crypto2 = res.author;
+      var crypto3 = res.description;
+      var crypto4 = res.url;
+      var link = $('<a class>').attr('href', crypto4).attr('class', 'article__link').text(crypto1);
+      link.a
+      var cryptfo = $('<div class ="crypto">').html(crypto1 + crypto2 + crypto3);
+      cryptoDiv.append(link);
+      
+     cryptoDiv.append(cryptImage);
+     cryptoDiv.append(cryptfo);
+   
+     $(".article__row").append(cryptoDiv);
+    
+    }
+   });
+};
+
+function calculateVolatility(data) {
+	var arr = []; 
+	for (i=0;i<(data.length-1);i++) {
+		interdayReturn = (data[i+1].price-data[i].price)/data[i].price;
+		arr.push(interdayReturn);
+	}
+	console.log(arr);
+	var stdArr = math.std(arr);
+	var annualized = Math.sqrt(365)*stdArr;
+	console.log(stdArr);
+	console.log(annualized);
+}
+
+
+makeArticles("Bitcoin");
+
 
